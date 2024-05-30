37c8836463756eb4536beae554edf1c35e52a507
Ирина Бабкина <i.p.ponomareva@yandex.ru>
2024-05-30 16:21:08 +0300

    my check

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = os.getenv('DEBUG_MODE', False).lower() == 'true'
+DEBUG = os.getenv('DEBUG_MODE', str(False)).lower() == 'true'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
