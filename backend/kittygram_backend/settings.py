67f540fa75505817d8f0ca4089cf1eff0a5a9789
Ирина Бабкина <i.p.ponomareva@yandex.ru>
2024-06-08 21:51:55 +0300

    --

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = get_random_secret_key()
 
 DEBUG = str(os.getenv('DEBUG', False)).lower() == 'true'
 
-# ALLOWED_HOSTS = ['158.160.88.118', '127.0.0.1', 'localhost', 'kittygramfall.zapto.org']
+ALLOWED_HOSTS = ['158.160.88.118', '127.0.0.1', 'localhost', 'kittygramfall.zapto.org']
 
 # SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
@@ -23,8 +23,8 @@ DEBUG = str(os.getenv('DEBUG', False)).lower() == 'true'
 
 # DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
-print(ALLOWED_HOSTS)
+
+# print(ALLOWED_HOSTS)
 # print(DEBUG)
 
 INSTALLED_APPS = [
