838a5281002668a8e4e42ffd2c965f313d43a4c1
Ирина Бабкина <i.p.ponomareva@yandex.ru>
2024-06-08 20:43:13 +0300

    hosts

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -19,7 +19,7 @@ DEBUG = True
 # DEBUG = os.getenv('DEBUG_MODE', str(False)).lower() == 'true'
 # DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1 localhost').split(' ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
