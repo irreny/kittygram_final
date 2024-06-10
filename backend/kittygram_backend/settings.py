fe936f76c72594f4a688fcd3f2b4bab921600609
Ирина Бабкина <i.p.ponomareva@yandex.ru>
2024-06-10 17:13:09 +0300

    hard

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,19 +8,19 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = get_random_secret_key()
+# SECRET_KEY = get_random_secret_key()
 
 DEBUG = True
 
 # DEBUG = str(os.getenv('DEBUG', False)).lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
-# SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
 # DEBUG = str(os.getenv('DEBUG', False)).lower() == 'true'
 
-# ALLOWED_HOSTS = ['158.160.88.118', '127.0.0.1', 'localhost', 'kittygramfall.zapto.org']
+ALLOWED_HOSTS = ['158.160.88.118', '127.0.0.1', 'localhost', 'kittygramfall.zapto.org']
 
 # ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
