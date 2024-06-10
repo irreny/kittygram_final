fa3a3f66e28965ca40162e15f4cfd41780874e0c
Ирина Бабкина <i.p.ponomareva@yandex.ru>
2024-06-10 13:35:22 +0300

    hosts+sk+deb

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,11 +8,18 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# SECRET_KEY = get_random_secret_key()
+
+# DEBUG = str(os.getenv('DEBUG', False)).lower() == 'true'
+
+# ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
 DEBUG = str(os.getenv('DEBUG', str(False))).lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = ['158.160.88.118', '127.0.0.1', 'localhost', 'kittygramfall.zapto.org']
+
 
 INSTALLED_APPS = [
     'django.contrib.admin',
